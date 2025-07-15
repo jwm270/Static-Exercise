18c90d66f8fd5e0d6f6e3be8096c6916d55f1639
Jimmy Moore <jwm270@gmail.com>
2025-07-14 21:10:50 -0500

    exercise complete

---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,15 @@
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            return (fahrenheit - 32) / 1.8;
+        }
+
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            return (celsius * 1.8) + 32;
+        }
+    }
+}
